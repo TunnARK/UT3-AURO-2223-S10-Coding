bf0a2cc95e8b5dc1f93ed6237402079f967396f4
TunnARK <96030186+TunnARK@users.noreply.github.com>
2022-12-09 10:51:03 +0100

    B2SATR TP1 Py: testing serial package (validated)

---
+++ SATR-TP1-ESPBank/client.py
@@ -0,0 +1,12 @@
+# -------------------------------------------- #
+# Class Client                                 #
+# Tasks:                                       #
+# - emit serial messages to the ESP card       #
+# - receive serial messages from the ESP card  #
+# -------------------------------------------- #
+
+import serial
+ser = serial.Serial('/dev/ttyUSB0') # open serial port
+print(ser.name)                     # check which port was really used
+ser.write(b'hello')                 # write a string
+ser.close()                         # close port
